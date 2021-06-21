65db589d3da624c88ae6542084abcea9d80ff165
Si Young Byun <siyoung.byun@gmail.com>
2021-06-21 14:43:07 -0500

    initial commit for Brighthive JWT

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+REQUIRED_PACKAGES = ["boto3", "pyjwt[crypto]"]
+
+setuptools.setup(
+    name="brighthive_jwt",
+    # version="0.0.1",
+    author="Brighthive",
+    author_email="engineering@brighthive.io",
+    description="Brighthive Library for JWT",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    # url="https://github.com/pypa/sampleproject",
+    # project_urls={
+    #     "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    # },
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=3.8",
+    install_requires=REQUIRED_PACKAGES,
+)

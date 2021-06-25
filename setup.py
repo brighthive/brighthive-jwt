89947d8769c117d87643b2f30bbe1cdfb8a0a4b3
Logan <logan@brighthive.io>
2021-06-25 12:13:35 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,24 @@
-import setuptools
+from distutils.core import setup
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
 
 REQUIRED_PACKAGES = ["boto3", "pyjwt[crypto]"]
 
-setuptools.setup(
+setup(
     name="bhjwt",
-    # version="0.0.1",
+    version="0.0.1",
     author="Brighthive",
-    author_email="engineering@brighthive.io",
-    description="Brighthive Library for JWT",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/brighthive/brighthive-jwt-authorization-lib",
-    classifiers=[
-        "Programming Language :: Python :: 3.8",
-        "Operating System :: OS Independent",
-    ],
+    # author_email="engineering@brighthive.io",
+    # description="Brighthive Library for JWT",
+    # long_description=long_description,
+    # long_description_content_type="text/markdown",
+    # url="https://github.com/brighthive/brighthive-jwt-authorization-lib",
+    # classifiers=[
+    #     "Programming Language :: Python :: 3.8",
+    #     "Operating System :: OS Independent",
+    # ],
     packages=['bhjwt'],
-    python_requires=">=3.8",
+    # python_requires=">=3.8",
     install_requires=REQUIRED_PACKAGES,
 )

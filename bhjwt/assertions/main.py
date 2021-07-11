e9742e28da11eea6e24d60efe7c6a4c13422d78d
Si Young Byun <siyoung.byun@gmail.com>
2021-07-11 17:37:49 -0500

    task: Assertions return Boolean

--- bhjwt/assertions/main.py
+++ bhjwt/assertions/main.py
@@ -3,40 +3,62 @@ from bhjwt.exceptions import AuthorizationError
 
 class AdminAssertions:
     def is_super_admin(self):
-        assertion = "brighthive-super-admin" in self.claims and self.claims["brighthive-super-admin"] == True
+        assertion = (
+            "brighthive-super-admin" in self.claims
+            and self.claims["brighthive-super-admin"] == True
+        )
         self.assertions.append(assertion)
+        return assertion
 
 
 class DataResourceAssertions:
     def _is_data_resource_role_of(self, data_resource_id: str, role: str) -> None:
-        assertion = "brighthive-data-resource-claims" in self.claims and self.claims[
-            "brighthive-data-resource-claims"].get(data_resource_id) is not None and \
-            self.claims["brighthive-data-resource-claims"][data_resource_id].get("role",
-                                                                                 "").lower() == role.lower()
+        assertion = (
+            "brighthive-data-resource-claims" in self.claims
+            and self.claims["brighthive-data-resource-claims"].get(data_resource_id)
+            is not None
+            and self.claims["brighthive-data-resource-claims"][data_resource_id]
+            .get("role", "")
+            .lower()
+            == role.lower()
+        )
         self.assertions.append(assertion)
+        return assertion
 
     def is_data_resource_admin_of(self, data_resource_id):
-        self._is_data_resource_role_of(data_resource_id, "admin")
+        assertion = self._is_data_resource_role_of(data_resource_id, "admin")
+        return assertion
 
     def is_data_resource_user_of(self, data_resource_id):
-        self._is_data_resource_role_of(data_resource_id, "user")
+        assertion = self._is_data_resource_role_of(data_resource_id, "user")
+        return assertion
 
-    def _has_permission_to_data_resource(self, data_resource_id: str, permission: str) -> None:
-        data_resource_claim = self.claims["brighthive-data-resource-claims"].get(data_resource_id)
+    def _has_permission_to_data_resource(
+        self, data_resource_id: str, permission: str
+    ) -> None:
+        data_resource_claim = self.claims["brighthive-data-resource-claims"].get(
+            data_resource_id
+        )
         data_perm = f"data:{permission.lower()}"
-        assertion = ("brighthive-data-resource-claims" in self.claims and
-                     data_resource_claim is not None and
-                     data_perm in data_resource_claim.get("permissions", []))
+        assertion = (
+            "brighthive-data-resource-claims" in self.claims
+            and data_resource_claim is not None
+            and data_perm in data_resource_claim.get("permissions", [])
+        )
         self.assertions.append(assertion)
+        return assertion
 
     def has_edit_to_data_resource(self, data_resource_id):
-        self._has_permission_to_data_resource(data_resource_id, "edit")
+        assertion = self._has_permission_to_data_resource(data_resource_id, "edit")
+        return assertion
 
     def has_view_to_data_resource(self, data_resource_id):
-        self._has_permission_to_data_resource(data_resource_id, "view")
+        assertion = self._has_permission_to_data_resource(data_resource_id, "view")
+        return assertion
 
     def has_download_to_data_resource(self, data_resource_id):
-        self._has_permission_to_data_resource(data_resource_id, "download")
+        assertion = self._has_permission_to_data_resource(data_resource_id, "download")
+        return assertion
 
 
 class AssertJwt(AdminAssertions, DataResourceAssertions):
@@ -51,5 +73,6 @@ class AssertJwt(AdminAssertions, DataResourceAssertions):
     def fail_if_none(self):
         if not any(self.assertions):
             raise AuthorizationError(
-                http_status_code=403, error_message="JWT does not have the necessary claims to perform this action.")
-
+                http_status_code=403,
+                error_message="JWT does not have the necessary claims to perform this action.",
+            )

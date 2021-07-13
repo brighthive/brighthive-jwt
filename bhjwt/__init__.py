c51dc88dab04d4acb2356026e65db21baed78334
Si Young Byun <siyoung.byun@gmail.com>
2021-07-12 20:12:13 -0500

    task: Make PublicKeys importable class

--- bhjwt/__init__.py
+++ bhjwt/__init__.py
@@ -1,4 +1,8 @@
-from bhjwt.main import create_asserter
+from bhjwt.main import create_asserter, PublicKeys
 from bhjwt.config import AuthLibConfiguration
-from bhjwt.providers import BrightHiveProvider, OAuth2ProviderFactory, OAuth2ProviderError
+from bhjwt.providers import (
+    BrightHiveProvider,
+    OAuth2ProviderFactory,
+    OAuth2ProviderError,
+)
 from bhjwt.decorators.token_required_decorator import token_required
